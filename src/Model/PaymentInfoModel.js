4f7c3263249403d0f55d4ecf4cde362f69bc7beb
Elegant Software Engineer <elegantdev199742@gmail.com>
2021-12-06 22:19:22 +0800

    feat(add): add payment info functionality

---
+++ src/Model/PaymentInfoModel.js
@@ -0,0 +1,34 @@
+let mongoose = require('mongoose')
+
+let paymentInfoSchema = new mongoose.Schema({
+    email : {
+        type : String,
+        require : true,
+    },
+    wallet : {
+        type : String,
+        require : true,
+    },
+    tokenId : {
+        type : String,
+        require : true,
+    },
+    status : {
+        type : String,
+        require : true,
+    },
+    ip : {
+        type : String,
+        require : true,
+    },
+    country_name : {
+        type : String,
+        require : true,
+    },
+  },{
+      timestamps : true,
+  });
+
+module.exports = {
+    paymentInfoModel : mongoose.model('PaymentInfo', paymentInfoSchema)
+}
