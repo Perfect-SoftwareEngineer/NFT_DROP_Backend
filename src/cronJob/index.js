6aba9279611cb4ab2dbb898a571582c03152fe24
Elegant Software Engineer <elegantdev199742@gmail.com>
2021-12-19 15:09:46 -0500

    remove this

--- src/cronJob/index.js
+++ src/cronJob/index.js
@@ -2,11 +2,13 @@ const cron = require("node-cron");
 const { runRoutine } = require("./threesCurry");
 
 const cronJob = () => {
-  const threesCurryTask = cron.schedule("*/1 * * * *", () => {
-    runRoutine();
-  });
-  
-  threesCurryTask.start();
+  if (process.env.NODE_ENV == 'production') {
+      const threesCurryTask = cron.schedule("*/1 * * * *", () => {
+        runRoutine();
+      });
+      
+      threesCurryTask.start();
+  }
 };
 
 module.exports = cronJob;
