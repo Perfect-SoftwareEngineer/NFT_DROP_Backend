f314df0923bdd358ac8dd0cdf79d7100b48f4c91
Elegant Software Engineer <elegantdev199742@gmail.com>
2022-05-06 02:54:22 +0800

    remove 3pt fetcher

--- src/cronJob/threesCurry.js
+++ src/cronJob/threesCurry.js
@@ -23,102 +23,6 @@ tpmCron = cron.schedule("*/1 * * * *", () => {
 });
 
 
-// 3pt twitter bot
-
-/**
- *
- * @param {number} playerId
- * @param {number} value
- */
- const storeTotalThreesInDB = (playerId, value) => {
-  threesCurryModel.updateOne(
-    { playerId },
-    {
-      playerId,
-      value,
-    },
-    { upsert: true }, (error, result) => {console.log(error, result)}
-  );
-};
-
-/**
- * @param {number} playerId => ThreesCurryModel
- */
-const checkDBValue = (playerId) => {
-    return threesCurryModel.findOne({ playerId });
-}
-
-
-/**
- *
- * @param {number} playerId
- * @param {(playerId: number, totalThrees: number) => void} cb
- */
- const tpmFetcher = (playerId, cb) => {
-  const options = {
-    method: "GET",
-    url: `https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/${playerId}`,
-    headers: {
-      "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-      "x-rapidapi-key": process.env.API_NBA_KEY,
-    },
-  };
-  
-  axios(options)
-    .then((response) => {
-      const currCount = {
-        489: 2899,
-      };
-
-      const { data } = response;
-      if (data.api && data.api.statistics.length) {
-        const { statistics } = data.api;
-        const newGames = statistics.slice(Object.keys(currCount)[0]);
-        let tpmCount = 0;
-        if (newGames.length) {
-          newGames.forEach((value, i) => {
-            let count = parseInt(value.tpm);
-
-            if (count) {
-              tpmCount += count;
-            }
-          });
-        }
-
-        const totalThrees = currCount["489"] + tpmCount;
-        cb(playerId, totalThrees);
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-};
-
-/**
- * Run subroutine to
- * 1. Fetch new TPM value
- * 2. Compare value to check if it needs to be updated/tweeted about
- * 3. Update/Tweet about it if necessary
- */
-const runTpmJob = () => {
-  tpmFetcher(124, async (playerId, value) => {
-      const document = await checkDBValue(playerId);
-      
-      if (!document) {
-          await threesCurryModel.create({ playerId: 124, value });
-          return;
-      };
-      
-      if (document.value !== value) {
-          // await addFreeMint(value - document.value);
-          await storeTotalThreesInDB(playerId, value);
-          
-          // await createTweet(`Steph Curry 3-point count: ${value}`);
-      }
-  });
-};
-
-
 
 const getLiveMatch = (cb) => {
   const optionlive = {
@@ -269,9 +173,6 @@ const setMerkleRoot = async (gameId) => {
   }
 }
 module.exports = {
-  tpmFetcher,
-  storeTotalThreesInDB,
-  runTpmJob,
   runLiveMatchJob,
   runTpmMatchJob
 };
